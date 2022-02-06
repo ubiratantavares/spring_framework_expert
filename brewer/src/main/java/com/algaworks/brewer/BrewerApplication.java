77d711c727c73115ec1317db61c576b5545070fd
= <=>
2022-02-06 01:06:02 -0300

    criando o projeto maven e configurando o arquivo pom.xml

---
+++ brewer/src/main/java/com/algaworks/brewer/BrewerApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.brewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrewerApplication.class, args);
+	}
+
+}
